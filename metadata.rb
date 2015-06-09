ea93e3f7ecdda74ce7f2256034d4d0a1057d914c
skubriev <skubriev@cvisionlab.com>
2015-06-09 17:21:57 +0300

    initial version

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'localsettings'
+maintainer       'RUGARD LLC'
+maintainer_email 'vladimir@skubriev.ru'
+license          'All rights reserved'
+description      'Installs/Configures localsettings'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends 'timezone-ii', '= 0.2.0'
